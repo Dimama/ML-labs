6af52dea060b36d0bfac2337a46cb3fe832b2600
Dimama <citrus_m@mail.ru>
2019-03-03 14:38:25 +0300

    Delete comments

--- l1/main.py
+++ l1/main.py
@@ -1,15 +1,9 @@
 from process_data import get_data_from_file
 from hard_drive_classifier import HardDriveClassifier
 from sklearn.neighbors import KNeighborsClassifier
-#import argparse
+
 
 if __name__ == "__main__":
-
-    #parser = argparse.ArgumentParser(description="Hard drive classifier")
-    #parser.add_argument("--data", help="File with data", metavar="filename", required=True)
-    #parser.add_argument("--load", help="File with trained classifier", metavar="filename", required=False)
-    #parser.add_argument("--save", help="File to save classifier", metavar="filename", required=False)
-    #parser.parse_args()
 
     data = get_data_from_file("harddrive1.arff")
     classifier = KNeighborsClassifier()
