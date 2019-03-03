7cd504a14d27ca7f958bd3ab1239c4fed63b630d
Dimama <citrus_m@mail.ru>
2019-03-03 14:26:14 +0300

    Add basic methods
    Add split data, fit and predict, load from file, save to file

--- l1/hard_drive_classifier.py
+++ l1/hard_drive_classifier.py
@@ -1,31 +1,78 @@
-class HardDriveClassifier:
-    def __init__(self, classifier, data, k=0.8):
-        self.classifier = classifier
-        self.data = data
-        self.k = k
-        self._get_additional_info()
+from random import shuffle
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.externals import joblib
 
-    def fit(self):
-        pass
+
+class HardDriveClassifier:
+    def __init__(self, data, classifier=None, k=0.8, classifier_file=None):
+
+        if classifier is not None:
+            self.classifier = classifier
+            self.is_fitted = False
+        else:
+            self.classifier = HardDriveClassifier._load_classifier(classifier_file)
+            self.is_fitted = True
+
+        self._split_data(data, k)
+        self._get_additional_info(data)
+
+    def fit(self, save=False):
+        self.classifier.fit(self.x_train, self.y_train)
+        self.is_fitted = True
+
+        if save:
+            self._save_to_file("classifier.saved")
 
     def predict(self):
-        pass
+        if not self.is_fitted:
+            raise Exception("Classifies is not fitted yet!")
+
+        predicted = self.classifier.predict(self.x_train)
+        print("Accuracy train:", accuracy_score(self.y_train, predicted))
+
+        predicted = self.classifier.predict(self.x_test)
+        print("Accuracy test:", accuracy_score(self.y_test, predicted))
+
+        print("Classification report:\n", classification_report(self.y_test,
+                                                                predicted,
+                                                                target_names=["not failed", "failed"]))
+        print("Confusion matrix:\n", confusion_matrix(self.y_test, predicted))
 
     def test(self, k=None):
         pass
 
-    def save(self, filename):
-        pass
+    def _save_to_file(self, filename):
+        if not self.is_fitted:
+            raise Exception("Classifies is not fitted yet!")
 
-    def load(self, filename):
-        pass
+        joblib.dump(self.classifier, filename)
+        print("Classifier saved to {}".format(filename))
 
-    def _get_additional_info(self):
-        self.p_count = self.data["y"].count(1)
-        self.n_count = self.data["y"].count(0)
+    def _split_data(self, data, k):
+        length = len(data["y"])
+        index = int(length * k)
+        indices = list(range(length))
+        shuffle(indices)
+
+        x = [data["x"][i] for i in indices]
+        y = [data["y"][i] for i in indices]
+
+        self.x_train, self.x_test = x[:index], x[index:]
+        self.y_train, self.y_test = y[:index], y[index:]
+
+    @staticmethod
+    def _load_classifier(filename):
+        try:
+            return joblib.load(filename)
+        except FileNotFoundError:
+            raise Exception("File {} not found".format(filename))
+
+    def _get_additional_info(self, data):
+        self.p_count = data["y"].count(1)
+        self.n_count = data["y"].count(0)
 
     def __repr__(self):
         return "Classifier:\n{0}\ndataset params:\n len: {1}\n 0/1 - {2}/{3}".format(self.classifier,
-                                                                                     len(self.data["x"]),
+                                                                                     self.n_count + self.p_count,
                                                                                      self.n_count,
                                                                                      self.p_count)
--- l1/main.py
+++ l1/main.py
@@ -1,10 +1,23 @@
 from process_data import get_data_from_file
 from hard_drive_classifier import HardDriveClassifier
-from sklearn import svm
+from sklearn.neighbors import KNeighborsClassifier
+#import argparse
 
 if __name__ == "__main__":
 
+    #parser = argparse.ArgumentParser(description="Hard drive classifier")
+    #parser.add_argument("--data", help="File with data", metavar="filename", required=True)
+    #parser.add_argument("--load", help="File with trained classifier", metavar="filename", required=False)
+    #parser.add_argument("--save", help="File to save classifier", metavar="filename", required=False)
+    #parser.parse_args()
+
     data = get_data_from_file("harddrive1.arff")
-    classifier = svm.SVC()
-    hd_classifier = HardDriveClassifier(classifier, data)
+    classifier = KNeighborsClassifier()
+    hd_classifier = HardDriveClassifier(data, classifier_file="classifier.saved", k=0.9)
     print(hd_classifier)
+
+    #hd_classifier.fit(save=True)
+    hd_classifier.predict()
+
+
+
